2e40417d49f659e88b2d008046d3ddca3df67218
Rob Fazio <reemfz@gmail.com>
2019-10-24 20:05:09 -0500

    initial commit - project setup

---
+++ iDine/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Rob Fazio on 10/24/19.
+//  Copyright © 2019 Rob Fazio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
