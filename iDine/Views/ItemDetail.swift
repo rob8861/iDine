b5e3d8de291cbd1e2389aab499119d9823a328c4
Rob Fazio <reemfz@gmail.com>
2019-10-24 21:25:03 -0500

    Build the Main Menu view, rows and detail view

---
+++ iDine/Views/ItemDetail.swift
@@ -0,0 +1,41 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Rob Fazio on 10/24/19.
+//  Copyright © 2019 Rob Fazio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    
+    var item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -25, y: -5)
+            }
+            Text(item.description)
+                .padding(.leading, 30)
+                .padding(.trailing, 30)
+            Spacer()
+        }
+        .navigationBarTitle(Text(item.name), displayMode: .inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ItemDetail(item: MenuItem.example)
+        }
+    }
+}
---
+++ iDine/Views/ItemRow.swift
@@ -0,0 +1,46 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Rob Fazio on 10/24/19.
+//  Copyright © 2019 Rob Fazio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    static let colors: [String:Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+    
+    var item: MenuItem
+    
+    var body: some View {
+        NavigationLink(destination: ItemDetail(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
+                Spacer()
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
